72bb60953b324de8f39618c64bfb2e33339e10a1
Sirius1616 <siriusa1.615@gmail.com>
2023-09-26 16:50:07 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class definition for square"""
+
+
+class square:
+    """Instantiation of the squre with size"""
+
+    def __init__(self, size):
+        """attributes of the instantiation is here"""
+
+        self.size = size

12d55b8772c53e0acce662abf1c797825068be14
Sirius1616 <siriusa1.615@gmail.com>
2023-09-26 17:32:50 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,13 +4,11 @@
 
 class square:
     """Instantiation of the squre with size base on 0-square.py
-       
        Attributes:
             size: size of square
         """
     def __init__(self, size):
         """creates a new instance of the square
-           
            Args:
                 size:one sided
 

609d7c3852ad8d67fd13b4fdaf38692bf9ee873b
Sirius1616 <siriusa1.615@gmail.com>
2023-10-11 16:51:44 -0700

    file input/output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """a function that writes a string to a text file (UTF8) 
+    and returns the number of characters written:"""
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        fw_file = file.write(text)
+        return(fw_file)
+

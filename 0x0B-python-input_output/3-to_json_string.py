62ed13c05c6d6d35d5ce81c2e8897b25b0498655
Sirius1616 <siriusa1.615@gmail.com>
2023-10-11 17:24:44 -0700

    file input/output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,4 +5,4 @@ import json
 
 def to_json_string(my_obj):
     """a function that returns the JSON representation of an object (string)"""
-    return(json.dumps(my_obj)
+    return(json.dumps(my_obj))

fd5156a0f15c00b728e1558ae0dd5511866774e8
Sirius1616 <siriusa1.615@gmail.com>
2023-10-16 18:09:53 -0700

    base case

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -59,7 +59,9 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if value <= 0:
+        if type(value) != int:
+            raise TypeError('x must be an integer')
+        if value < 0:
             raise ValueError('x must be >= 0')
         self.__x = value
 
@@ -71,7 +73,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if value <= 0:
+        if type(value) != int:
+            raise TypeError('y must be an integer')
+        if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
     

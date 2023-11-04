c3df1fadda9113ed97b54cba5378499aa818b21b
Sirius1616 <siriusa1.615@gmail.com>
2023-11-04 02:13:02 -0700

    class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""A class for square"""
+
+
 class Square:
     """
     Class that defines properties of square by: (based on 1-square.py).
-
-    Attributes:
-        size: size of a square (1 side).
     """
     def __init__(self, size=0):
         """Creates new instances of square.
@@ -18,8 +18,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         """

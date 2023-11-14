450897465313cdff6e79cb9ccf907ae807a2ec1d
Sirius1616 <siriusa1.615@gmail.com>
2023-11-14 14:18:49 -0800

    mysql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
---Deleting a database
---Query that deletes the database hbtn_0c_0 in your MySQL server.
+-- Deleting a database
+-- Query that deletes the database hbtn_0c_0 in your MySQL server
 
 DROP DATABASE IF EXISTS hbtn_0c_0;

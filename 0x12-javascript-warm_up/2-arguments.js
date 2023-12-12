902164cc46f0bdeb30f686516e71404a0701c3af
Sirius1616 <siriusa1.615@gmail.com>
2023-12-12 14:11:13 -0800

    JS warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const myvar = process.argv.length - 2;
-console.log(myvar === 1 ? 'No argument' : myvar === 2 ? 'Argument found' : 'Arguments found');
+console.log(myvar === 2 ? 'No argument' : myvar === 3 ? 'Argument found' : 'Arguments found');

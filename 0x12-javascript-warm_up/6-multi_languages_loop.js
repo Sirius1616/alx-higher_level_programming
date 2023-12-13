b0d7553edc0ce09c66e0f1f54faa400c1d567dd9
Sirius1616 <siriusa1.615@gmail.com>
2023-12-12 17:03:31 -0800

    JS warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (item of array){
 	console.log(item);
 }

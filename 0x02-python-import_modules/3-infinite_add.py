dd83e734f31faa7b93dd41d7de026ff7f1d15858
Sirius1616 <siriusa1.615@gmail.com>
2024-02-05 14:56:53 -0800

    infinit add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
 	import sys
 	arg = sys.argv
-	sum = 0
-	for i in range(len(arg)):
-		sum += int(arg[i])
-	print(sum)	
+	_sum = 0
+	for i in range(1, len(arg)):
+		_sum += int(arg[i])
+	print(_sum)	

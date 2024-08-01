dbb19f09f69df5247f54e86e768c0a0ac1ee53ff
Sirius1616 <siriusq1.615@gmail.com>
2024-07-31 19:42:45 -0700

    Web scrapping file writting

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const _string = process.argv[3];
+
+fs.writeFile(filename, _string, 'utf-8', (error) => {
+    if(error){
+        console.log(error)
+    }
+})
